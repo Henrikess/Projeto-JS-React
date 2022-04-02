3fa83a39d62b34552dbae30d05257066a4ad2c51
Luis Henrique S <luishenriquesantana92@gmail.com>
2022-04-02 11:51:57 -0300

    Criando arquivos do projeto em JS, React e estrutura de dados.

---
+++ js/funcoes.js
@@ -0,0 +1,27 @@
+alert("Seja bem vindos a noss página com Javascript!")
+
+var nome;
+nome=prompt("Olá, qual seu nome?","Digite o seu nome...");
+idade=prompt("... E aqual sua idade?","Digite o sua idade...");
+email=prompt("Me informe, agora, o seu email:","Insira seu email...");
+function msg(){
+    alert("Você clicou no botão,"+nome+"! Você tem"+idade+" e seu email é "+email+".");
+
+
+
+}
+
+
+//Criando variavel em js.
+
+//var msg= "Essa é uma mensagem criada em uma variavel JS"
+//alert(msg)*/
+
+//Criando variavel e intervindo com JS
+
+//alert("Prazer em conhece-lo, "+nome+"!");
+
+//Escrevendo a interação na página WEB
+
+//document.write("Prazer em conhece-lo, "+nome+"!");  
+//.write("Legal seu nome, "+nome+"!");  
